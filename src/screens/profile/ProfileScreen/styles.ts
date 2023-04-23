f2c40bdea675868c6a3338b1dae92e765781e8b3
Chimwemwe Phikiso <33883304+cphikiso@users.noreply.github.com>
2023-04-23 07:33:19 +0100

    profile styles clean up

--- src/screens/profile/ProfileScreen/styles.ts
+++ src/screens/profile/ProfileScreen/styles.ts
@@ -6,4 +6,42 @@ export const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingHorizontal: 16,
   },
+  listHeader: { flexDirection: "row", marginBottom: 16, paddingTop: 16 },
+  profilePic: {
+    height: 80,
+    width: 80,
+    borderRadius: 80,
+    marginRight: 16,
+  },
+  nameBio: { marginTop: 8 },
+  name: {
+    fontFamily: "SFProRoundedHeavy",
+    fontSize: 22,
+  },
+  bio: {
+    fontFamily: "SFProRoundedMedium",
+    fontSize: 16,
+  },
+  followFollowing: { flexDirection: "row", marginBottom: 16 },
+  followText: {
+    fontFamily: "SFProRoundedMedium",
+    fontSize: 18,
+    marginRight: 16,
+  },
+  inviteBorder: { height: 1, backgroundColor: "#F5F5F5", marginBottom: 8 },
+  inviteButton: {
+    height: 58,
+    borderRadius: 58,
+    width: 192,
+    borderWidth: 2,
+    borderColor: "rgba(60,60,67,0.3)",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 8,
+  },
+  inviteText: {
+    fontFamily: "SFProRoundedHeavy",
+    fontSize: 18,
+    color: "rgba(60,60,67,0.3)",
+  },
 });
