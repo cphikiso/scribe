d26f346bf25f3911cb3c0697b5755143c543a6ad
Chimwemwe Phikiso <33883304+cphikiso@users.noreply.github.com>
2023-05-03 18:34:10 +0100

    edit profile setup

--- src/screens/profile/EditProfile/EditProfileScreen.tsx
+++ src/screens/profile/EditProfile/EditProfileScreen.tsx
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
 import React from "react";
+import { styles } from "./styles";
 
 const EditProfileScreen = () => {
   return (
-    <View>
-      <Text>EditProfileScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <TouchableOpacity style={styles.imageContainer}>
+        <Image
+          source={require("../../../../assets/pic.jpg")}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+
+      <View>
+        <View>
+          <Text style={styles.label}>FullName</Text>
+          <Text style={styles.input}>John Doe</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
---
+++ src/screens/profile/EditProfile/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 36,
+    paddingHorizontal: 16,
+  },
+  imageContainer: {
+    height: 100,
+    width: 100,
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  image: { height: 100, width: 100, borderRadius: 60 },
+});
